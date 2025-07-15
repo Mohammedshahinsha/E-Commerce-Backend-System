c37da2937584c59c47c4743a06923a6b996a4437
muhammed <119309213+Mohammedshahinsha@users.noreply.github.com>
2025-07-15 14:16:53 +0530

    Add files via upload

---
+++ src/test/java/com/project321/ECommerceBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project321;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ECommerceBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
