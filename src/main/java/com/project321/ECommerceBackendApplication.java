7da8d5d57fbd67573786b022d597beb8f2c6ae63
muhammed <119309213+Mohammedshahinsha@users.noreply.github.com>
2025-07-15 18:07:03 +0530

    Update ECommerceBackendApplication.java

--- src/main/java/com/project321/ECommerceBackendApplication.java
+++ src/main/java/com/project321/ECommerceBackendApplication.java
@@ -1,5 +1,5 @@
 package com.project321;
-
+import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
